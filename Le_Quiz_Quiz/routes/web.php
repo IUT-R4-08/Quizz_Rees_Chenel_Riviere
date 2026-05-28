35ee5f480dc654b68abfe38ba2ee41773686bfae
Kyno3146 <kyllian_riviere@etu.u-bourgogne.fr>
2026-05-28 10:47:04 +0200

    Template quiz

--- Le_Quiz_Quiz/routes/web.php
+++ Le_Quiz_Quiz/routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/quiz/test', function () {
+    // On simule ce que la BDD renverra plus tard
+    $quizFakeData = [
+        'title' => 'Quiz Culture Web',
+        'questions' => [
+            [
+                'id' => 1,
+                'text' => 'Que signifie l\'acronyme CSS ?',
+                'choices' => [
+                    'Cascading Style Sheets',
+                    'Creative Style System',
+                    'Computer Science Symbols'
+                ]
+            ],
+            [
+                'id' => 2,
+                'text' => 'Quel langage utilise le framework Laravel ?',
+                'choices' => [
+                    'Python',
+                    'PHP',
+                    'JavaScript',
+                    'Ruby'
+                ]
+            ]
+        ]
+    ];
+
+    // On envoie les données à la vue 'quiz'
+    return view('quiz', ['quiz' => $quizFakeData]);
 });
